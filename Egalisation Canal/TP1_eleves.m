03ef7221d356477d52efd560e1f96e15ece47d42
antoinetelescope <lescopantoine@gmail.com>
2024-12-04 13:10:26 +0100

    version quasi finale code

--- Egalisation Canal/TP1_eleves.m
+++ Egalisation Canal/TP1_eleves.m
@@ -25,13 +25,13 @@ clear all;
 %Frame length
 M=4; %2:BPSK, 4: QPSK
 N  = 10000; % Number of transmitted bits or symbols
-Es_N0_dB = [0:30]; % Eb/N0 values
+Es_N0_dB = [0:3:30]; % Eb/N0 values
 %Multipath channel parameters
 hc=[1 0.8*exp(1i*pi/3) 0.3*exp(1i*pi/6) ];%0.1*exp(1i*pi/12)];%ISI channel
 % hc=[0.04, -0.05, 0.07, -0.21, -0.5, 0.72, 0.36, 0, 0.21, 0.03, 0.07];
 %hc=[0.407, 0.815, 0.407]
 %hc = [0.227, 0.46, 0.688, 0.460, 0.227];
-a=0.2;
+%a=0.2;
 %hc=[1 -a];
 Lc=length(hc);%Channel length
 ChannelDelay=0; %delay is equal to number of non causal taps
@@ -96,9 +96,9 @@ for ii = 1:length(Es_N0_dB)
     %%MMSE
 
     deltac= zeros(1,2*Lc-1);
-    deltac (Lc) = 1 ;
+    deltac(Lc) = 1 ;
     Nmmse = 200;% non causal
-    [r,p,k]=residuez(fliplr(conj(hc)),(conv(hc,fliplr(conj(hc)))+(sig2b / sigs2) *deltac ) ) ;
+    [r,p,k]=residuez(fliplr(conj(hc)),(conv(hc,fliplr(conj(hc)))+(sig2b/sigs2)*deltac));
     [w_mmseinf]=ComputeRI(Nmmse,r,p,k);
     s_mmse=conv(w_mmseinf,y);
 
@@ -121,7 +121,7 @@ for ii = 1:length(Es_N0_dB)
 
    %% FIR ZF
 
-    Nw=10;
+    Nw=12;
     d = 5;          %distance
     H = toeplitz([hc(1) zeros(1,Nw-1)]',[hc, zeros(1,Nw-1)]);
     Ry = (conj(H)*H.');
@@ -134,7 +134,7 @@ for ii = 1:length(Es_N0_dB)
     Gamma = conj(H)*p;
     w_zf_fir = (inv(Ry)*Gamma).';
 
-    sig_e_opt = sigs2 -conj(w_zf_fir)*Gamma;
+    sig_e_opt = sigs2-conj(w_zf_fir)*Gamma;
     bias = 1-sig_e_opt/sigs2;
     shat = conv(w_zf_fir,y);
     shat = shat(dopt:end);
@@ -147,7 +147,7 @@ for ii = 1:length(Es_N0_dB)
 
     %% FIR MMSE
 
-    Nw = 10;
+    Nw = 12;
     d=5;
     H = toeplitz([hc(1) zeros(1,Nw-1)]',[hc, zeros(1,Nw-1)]);
     Ry = sigs2*(conj(H)*H.')+sig2b*eye(Nw);
@@ -181,10 +181,12 @@ end
 
 simBer_mmseinf = nErr_mmseinf/N/log2(M);
 simBer_mmseinfdirectimp = nErr_mmseinfdirectimp/N/log2(M);
+
 simBer_zfinfdirectimp = nErr_zfinfdirectimp/N/log2(M); 
 simBer_zfinf = nErr_zfinf/N/log2(M); 
 
 simBer_zf_ls= nErr_zf_ls/N/log2(M);
+
 simBer_mmse_ls= nErr_mmse_ls/N/log2(M);
 
 
@@ -192,27 +194,28 @@ simBer_mmse_ls= nErr_mmse_ls/N/log2(M);
 
 figure
 
-semilogy(Es_N0_dB,simBer_mmse_ls(1,:),'p-','Linewidth',2);
-hold on
-semilogy(Es_N0_dB,simBer_zf_ls(1,:),'bs-','Linewidth',2);
-hold on
+
 semilogy(Es_N0_dB,simBer_zfinfdirectimp(1,:),'p-','Linewidth',2);
-hold on
+% hold on
 % semilogy(Es_N0_dB,simBer_zfinf(1,:),'bs-','Linewidth',2);
 hold on
 semilogy(Es_N0_dB,simBer_mmseinfdirectimp(1,:),'p-','Linewidth',2);
 hold on
+semilogy(Es_N0_dB,simBer_zf_ls(1,:),'p-','Linewidth',2);
+hold on
+semilogy(Es_N0_dB,simBer_mmse_ls(1,:),'p-','Linewidth',2);
+% hold on
 % semilogy(Es_N0_dB,simBer_mmseinf(1,:),'rs-','Linewidth',2);
-axis([0 50 10^-6 0.5])
+axis([0 40 10^-6 0.5])
 grid on
-legend('sim-zf-inf/indirect','sim-mmse-inf/indirect');
+legend('sim-zf-inf/direct','sim-mmse-inf/direct','sim-zf-rif','sim-mmse-rif');
 xlabel('E_s/N_0, dB');
 ylabel('Bit Error Rate');
 title('Bit error probability curve for QPSK in ISI with ZF and MMSE equalizers')
 
 figure
 title('Impulse response')
-stem(real(w_zfinf))
+stem(real(w_mmseinf))
 hold on
 stem(real(w_zfinf),'r-')
 ylabel('Amplitude');
